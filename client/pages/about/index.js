1e506a488d9d74552413a27780b7ae0b0c35b4ae
Shakiba448 <shakiba448@gmail.com>
2021-05-31 03:53:12 +0600

    WIP on DJ EVENTS

--- client/pages/about/index.js
+++ client/pages/about/index.js
@@ -1,7 +1,9 @@
 export default function AboutPage() {
   return (
     <div>
-      <h1>About page</h1>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other musical events</p>
+      <p>version 1.0.0</p>
     </div>
   );
 }
