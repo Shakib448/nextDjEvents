0dd7565c90312167e3a315ed6b3677b4dbe75248
Shakiba448 <shakiba448@gmail.com>
2021-05-31 04:21:29 +0600

    WIP on DJ EVENTS

--- client/pages/events/[slug].js
+++ client/pages/events/[slug].js
@@ -1,7 +1,9 @@
+import Layout from "../../components/Layout";
+
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My Events</h1>
-    </div>
+    </Layout>
   );
 }
