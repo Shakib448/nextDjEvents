c7169cd9e527eb3aac6a899e0798ed94f7085209
Shakiba448 <shakiba448@gmail.com>
2021-06-05 19:34:48 +0600

    WIP on API

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,7 +10,7 @@ export default function HomePage({ events }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const { data } = await axiosConfig.get("/api/events");
-  return { props: { events: data } };
+  return { props: { events: data }, revalidate: 1 };
 }
