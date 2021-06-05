8ffbd34ed080cd5aa5e36aa4f99639608f712192
Shakiba448 <shakiba448@gmail.com>
2021-06-05 21:21:56 +0600

    WIP on API

--- client/pages/events/[slug].js
+++ client/pages/events/[slug].js
@@ -1,9 +1,16 @@
 import Layout from "@/components/Layout";
+import axiosConfig from "../../config";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
+  const singleEvents = Object.assign({}, ...evt);
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>{singleEvents.name}</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const { data } = await axiosConfig.get(`/api/events/${slug}`);
+  return { props: { evt: data } };
 }
