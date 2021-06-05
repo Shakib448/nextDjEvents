eaa298714d6abe5ad005237f6bb410616b8d70a0
Shakiba448 <shakiba448@gmail.com>
2021-06-05 21:28:43 +0600

    WIP on API

--- client/pages/events/[slug].js
+++ client/pages/events/[slug].js
@@ -10,7 +10,21 @@ export default function EventPage({ evt }) {
   );
 }
 
-export async function getServerSideProps({ query: { slug } }) {
-  const { data } = await axiosConfig.get(`/api/events/${slug}`);
-  return { props: { evt: data } };
+export async function getStaticPaths() {
+  const { data } = await axiosConfig.get(`/api/events`);
+
+  const paths = data.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+  return { paths, fallback: true };
 }
+
+export async function getStaticProps({ params: { slug } }) {
+  const { data } = await axiosConfig.get(`/api/events/${slug}`);
+  return { props: { evt: data }, revalidate: 1 };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const { data } = await axiosConfig.get(`/api/events/${slug}`);
+//   return { props: { evt: data } };
+// }
