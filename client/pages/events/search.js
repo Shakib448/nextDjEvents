54611769e63b7c30f36f200cf811fee0ec9be784
Shakiba448 <shakiba448@gmail.com>
2021-06-08 15:56:34 +0600

    Push Addon Added

--- client/pages/events/search.js
+++ client/pages/events/search.js
@@ -1,12 +1,13 @@
 import Layout from "@/components/Layout";
 import axiosConfig from "../../config";
 import EventItem from "@/components/EventItem";
+import qs from "qs";
 
 export default function SearchPage({ events }) {
   return (
     <Layout>
-      <h1> Events</h1>
-      {events?.length === 0 && <h3>No events to show</h3>}
+      <h1>Search Result</h1>
+      {events?.length === 0 && <h3>No result found</h3>}
       {events?.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
@@ -15,6 +16,16 @@ export default function SearchPage({ events }) {
 }
 
 export async function getServerSideProps({ query: { term } }) {
-  const { data } = await axiosConfig.get(`/events?name_contains=${term}`);
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { performarce_contains: term },
+        { description_contains: term },
+        { venue_contains: term },
+      ],
+    },
+  });
+  const { data } = await axiosConfig.get(`/events?${query}`);
   return { props: { events: data } };
 }
