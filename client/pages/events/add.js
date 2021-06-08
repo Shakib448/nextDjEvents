de9860b930b8645cf4820d1defdfb5706195f46a
Shakiba448 <shakiba448@gmail.com>
2021-06-09 05:37:57 +0600

    Add stapi event

--- client/pages/events/add.js
+++ client/pages/events/add.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import axiosConfig from "../../config";
+import styles from "@/styles/Form.module.scss";
 import Layout from "@/components/Layout";
+import clsx from "clsx";
 
 export default function AddEventsPage() {
+  const [values, setValues] = useState({
+    name: "",
+    performance: "",
+    venue: "",
+    address: "",
+    date: "",
+    time: "",
+    description: "",
+  });
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <Layout title="Add New Event">
+      <Link href="/events">Go Back</Link>
       <h1>Add Events</h1>
+
+      <form onSubmit={handleSubmit} className={clsx(styles.form)}>
+        <div className={clsx(styles.grid)}>
+          <div>
+            <label htmlFor="name">Event Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 }
