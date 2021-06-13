29106c8d72bd323d7190dac415cf24b190aaeff9
Shakiba448 <shakiba448@gmail.com>
2021-06-13 14:48:06 +0600

    WIP on Pagination

--- client/pages/events/index.js
+++ client/pages/events/index.js
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout";
 import axiosConfig from "../../config";
 import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
 const PER_PAGE = 2;
 
-export default function EventPage({ events }) {
+export default function EventPage({ events, page, total }) {
+  const lastPage = Math.ceil(total / PER_PAGE);
+
   return (
     <Layout>
       <h1> Events</h1>
@@ -12,6 +15,16 @@ export default function EventPage({ events }) {
       {events?.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+      {page > 1 && (
+        <Link href={`/events?page=${page - 1}`}>
+          <a className="btn-secondary">Prev</a>
+        </Link>
+      )}
+      {page < lastPage && (
+        <Link href={`/events?page=${page + 1}`}>
+          <a className="btn-secondary">Next</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -21,8 +34,13 @@ export async function getServerSideProps({ query: { page = 1 } }) {
 
   const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
 
+  // Fetch Total
+  const totalRes = await axiosConfig.get(`/events/count`);
+
+  // Fetch events
   const { data } = await axiosConfig.get(
     `/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
   );
-  return { props: { events: data } };
+
+  return { props: { events: data, page: +page, total: totalRes.data } };
 }
