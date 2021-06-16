28c315086910ad3c8a0d58c07f9e6994c5ef4a62
Shakiba448 <shakiba448@gmail.com>
2021-06-17 05:26:50 +0600

    WIP on Functionality

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,8 +3,11 @@ import styles from "@/styles/Header.module.scss";
 import clsx from "clsx";
 import Search from "./Search";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import AuthContext from "@/context/AuthContext";
+import { useContext } from "react";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={clsx(styles.header)}>
       <div className={clsx(styles.logo)}>
@@ -20,19 +23,33 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="btn-secondary btn-icon"
+                  onClick={() => logout()}
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
