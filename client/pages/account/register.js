4e5ac4e42104eb263005e4fdd67c8fe9b80982c9
Shakiba448 <shakiba448@gmail.com>
2021-06-16 20:35:21 +0600

    WIP on LoginPage

--- client/pages/account/register.js
+++ client/pages/account/register.js
@@ -1,7 +1,68 @@
+import { FaUser } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import styles from "@/styles/AuthForm.module.scss";
+import clsx from "clsx";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, email, password });
+  };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Layout title="User Registration">
+      <div className={clsx(styles.auth)}>
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">User Name </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <input type="submit" value="Login" className="btn" />
+        </form>
+
+        <p>
+          Don't have an account? <Link href="/account/register"> Register</Link>
+        </p>
+      </div>
+    </Layout>
   );
 }
