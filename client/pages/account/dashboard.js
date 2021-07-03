7c7908b0fc42a1357c728a01993d602de5553957
Shakiba448 <shakiba448@gmail.com>
2021-07-03 09:28:12 +0600

    WIP on strapi-events

--- client/pages/account/dashboard.js
+++ client/pages/account/dashboard.js
@@ -1,12 +1,55 @@
+import DashboardEvent from "@/components/DashboardEvent";
 import Layout from "@/components/Layout";
 import { parseCookie } from "@/helpers/index";
+import clsx from "clsx";
 import axiosConfig from "../../config";
+import styles from "@/styles/Dashboard.module.scss";
+import { useRouter } from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function dashboard({ events }) {
-  console.log(events);
+export default function dashboard({ events, token }) {
+  const router = useRouter();
+  const deleteEvent = async (id) => {
+    try {
+      await axiosConfig.delete(`/events/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      toast.error(error.response.data.message);
+      router.reload();
+    }
+    // if (confirm('Are you sure?')) {
+    //   const res = await fetch(`${API_URL}/events/${id}`, {
+    //     method: 'DELETE',
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   })
+
+    //   const data = await res.json()
+
+    //   if (!res.ok) {
+    //     toast.error(data.message)
+    //   } else {
+    //     router.reload()
+    //   }
+    // }
+    console.log("delete");
+  };
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={clsx(styles.dash)}>
+        <ToastContainer />
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
@@ -17,6 +60,6 @@ export async function getServerSideProps({ req }) {
     headers: { Authorization: `Bearer ${token}` },
   });
   return {
-    props: { events: data },
+    props: { events: data, token },
   };
 }
